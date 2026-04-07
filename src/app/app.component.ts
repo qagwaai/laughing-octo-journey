e1032cfc24c95ad96d96905209de1e0735397f21
Peter Girard <peter.girard@swisslog-healthcare.com>
2026-04-07 14:12:43 -0600

    add stats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component, ElementRef, inject, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, inject, signal } from '@angular/core';
 import { TweakpaneButton, TweakpaneCheckbox, TweakpaneColor, TweakpanePane } from 'angular-three-tweakpane';
 import { NgtCanvas } from 'angular-three/dom';
 import { Experience } from './experience/experience.component';
 import { SceneGraph } from './scene-graph';
+import { NgtsStats } from 'angular-three-soba/stats';
 
 // <app-experience *canvasContent />
 @Component({
   selector: 'app-root',
   template: `
     <ngt-canvas
+      [stats]="{ parent: host, domClass: 'stats' }"
       shadows 
       [camera]="{ position: [5, 5, 5] }" 
       [lookAt]="[0, 1, 0]"
@@ -26,8 +28,17 @@ import { SceneGraph } from './scene-graph';
     </tweakpane-pane>
   `,
   host: { class: 'block h-dvh w-full' },
-  styles: `:host { display: block; height: 100vh; width: 100vw; background: #050208; }`,
-  imports: [NgtCanvas, Experience, SceneGraph, TweakpanePane, TweakpaneCheckbox, TweakpaneColor, TweakpaneButton],
+  styles: `:host { display: block; height: 100vh; width: 100vw; background: #050208;
+    & .stats {
+				position: static !important;
+
+				& canvas {
+					margin-top: 0 !important;
+				}
+			}
+  }`,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], 
+  imports: [NgtCanvas, Experience, SceneGraph, TweakpanePane, TweakpaneCheckbox, TweakpaneColor, TweakpaneButton, NgtsStats],
 })
 export class AppComponent {
   protected host = inject(ElementRef);
